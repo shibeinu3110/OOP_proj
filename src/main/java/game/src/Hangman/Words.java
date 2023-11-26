142104a9cc4ddcc70c4b4ad6fa4a8780eef3b5ba
xxpppddd <118286404+xxpppddd@users.noreply.github.com>
2023-11-26 13:55:29 +0700

    game

---
+++ src/main/java/game/src/Hangman/Words.java
@@ -0,0 +1,29 @@
+package game.src.Hangman;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Words {
+
+    private final ArrayList<String> words;
+
+
+    public Words() throws FileNotFoundException {
+        words = new ArrayList<>();
+        File file = new File("hangmanwords.txt");
+        Scanner scanner = new Scanner(file);
+        String blankLine = scanner.nextLine();
+        while (scanner.hasNextLine()) {
+            String word = scanner.nextLine();
+            words.add(word);
+        }
+    }
+
+    public String getRandomWord() {
+        int randomIndex = (int) (Math.random() * words.size());
+        return words.get(randomIndex).toUpperCase();
+    }
+
+}
